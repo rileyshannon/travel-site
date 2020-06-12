5985dd43b477689b32ab2ea0d49d529a28efde1d
Riley Shannon <riley.shannon@protonmail.com>
2020-06-12 14:48:49 -0500

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,27 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
-let mobileMenu = new MobileMenu();
+
+new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
-let stickyHeader = new StickyHeader();
+new StickyHeader();
+
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(el => {
+   el.addEventListener("click", e => {
+       e.preventDefault();
+       if (typeof modal == "undefined") {
+           import(/* webpackChunkName: "modal" */"./modules/Modal").then(x => {
+               modal = new x.default();
+               setTimeout(() => modal.openModal(), 20);
+           }).catch(() => console.log("There was a problem loading the Modal module"));
+       } else {
+           modal.openModal();
+       }
+   });
+});
 
 if (module.hot) {
     module.hot.accept();
